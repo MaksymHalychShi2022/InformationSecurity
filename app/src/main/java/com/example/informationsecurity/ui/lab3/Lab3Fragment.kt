96bbf06f977ce9c25339e9b64f64ab39c094d25e
MaksHalych <maksym.halych.shi.2022@lpnu.ua>
2024-10-20 20:51:39 +0300

    add stubs for lab3

--- app/src/main/java/com/example/informationsecurity/ui/lab3/Lab3Fragment.kt
+++ app/src/main/java/com/example/informationsecurity/ui/lab3/Lab3Fragment.kt
@@ -1,39 +1,112 @@
 package com.example.informationsecurity.ui.lab3
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import com.example.informationsecurity.MainViewModel
 import com.example.informationsecurity.databinding.FragmentLab3Binding
+import com.example.informationsecurity.utils.OperationState
 
 class Lab3Fragment : Fragment() {
 
+    private lateinit var encryptFilePickerLauncher: ActivityResultLauncher<Intent>
+    private lateinit var decryptFilePickerLauncher: ActivityResultLauncher<Intent>
     private var _binding: FragmentLab3Binding? = null
+    private lateinit var lab3ViewModel: Lab3ViewModel
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val lab3ViewModel =
-            ViewModelProvider(this).get(Lab3ViewModel::class.java)
+        lab3ViewModel = ViewModelProvider(this).get(Lab3ViewModel::class.java)
 
         _binding = FragmentLab3Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        lab3ViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.btnEncryptFile.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "*/*"  // You can change this MIME type if you want to filter file types
+            }
+            encryptFilePickerLauncher.launch(intent)
+        }
+
+
+        binding.btnDecryptFile.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "*/*"  // You can change this MIME type if you want to filter file types
+            }
+            decryptFilePickerLauncher.launch(intent)
         }
         return root
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        encryptFilePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val uri: Uri? = result.data?.data
+                    uri?.let {
+                        // Process the selected file's Uri for reading
+                        lab3ViewModel.encryptFile(uri).observe(viewLifecycleOwner) { result ->
+                            observeForProgressBar(result, "Encrypted!")
+                        }
+                    }
+                }
+            }
+
+        decryptFilePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val uri: Uri? = result.data?.data
+                    uri?.let {
+                        // Process the selected file's Uri for reading
+                        lab3ViewModel.decryptFile(uri).observe(viewLifecycleOwner) { result ->
+                            observeForProgressBar(result, "Decrypted!")
+                        }
+                    }
+                }
+            }
+    }
+
+
+    private fun observeForProgressBar(result: OperationState<*>, successMassage: String? = null) {
+        when (result) {
+            is OperationState.Loading -> {
+                mainViewModel.showProgressBar()
+            }
+
+            is OperationState.Success -> {
+                successMassage?.let {
+                    Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                }
+                mainViewModel.hideProgressBar()
+            }
+
+            is OperationState.Error -> {
+                Toast.makeText(requireContext(), result.message, Toast.LENGTH_SHORT).show()
+                mainViewModel.hideProgressBar()
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/informationsecurity/ui/lab3/Lab3ViewModel.kt
+++ app/src/main/java/com/example/informationsecurity/ui/lab3/Lab3ViewModel.kt
@@ -1,13 +1,82 @@
 package com.example.informationsecurity.ui.lab3
 
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
+import android.net.Uri
+import android.content.ContentResolver
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.informationsecurity.utils.OperationState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.OutputStream
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+import javax.crypto.KeyGenerator
+import javax.crypto.spec.IvParameterSpec
+import java.security.SecureRandom
 
-class Lab3ViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Lab 3 Fragment"
+class Lab3ViewModel(application: Application) : AndroidViewModel(application) {
+    private val _passphrase = MutableLiveData<String>().apply {
+        value = "This is the pass phrase"
     }
-    val text: LiveData<String> = _text
+    val passphrase: LiveData<String> = _passphrase
+
+
+    fun encryptFile(
+        inputUri: Uri,
+        wordSize: Int = 64,
+        rounds: Int = 12,
+        keyLength: Int = 8
+    ): LiveData<OperationState<Unit>> {
+        val operationState = MutableLiveData<OperationState<Unit>>()
+        viewModelScope.launch {
+            operationState.postValue(OperationState.Loading())  // Show loading state
+
+            withContext(Dispatchers.IO) {
+                try {
+                    delay(2000)
+
+                    operationState.postValue(OperationState.Success(Unit))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    operationState.postValue(OperationState.Error("Error: ${e.message}"))  // Error occurred
+                }
+            }
+        }
+        return operationState
+    }
+
+    fun decryptFile(
+        inputUri: Uri,
+        wordSize: Int = 64,
+        rounds: Int = 12,
+        keyLength: Int = 8
+    ): LiveData<OperationState<Unit>> {
+        val operationState = MutableLiveData<OperationState<Unit>>()
+        viewModelScope.launch {
+            operationState.postValue(OperationState.Loading())  // Show loading state
+
+            withContext(Dispatchers.IO) {
+                try {
+                    delay(2000)
+
+                    operationState.postValue(OperationState.Success(Unit))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    operationState.postValue(OperationState.Error("Error: ${e.message}"))  // Error occurred
+                }
+            }
+        }
+        return operationState
+    }
+
 }
